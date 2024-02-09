bd653c50043daee0a9415d128fef6bfbf6a5baa4
Yasmim-bs <yasmimsabrinacp@gmail.com>
2024-02-08 22:46:39 -0300

    feat: adding question and alternatives entities

---
+++ src/main/java/com/nlw/certification/questions/AlternativesEntity.java
@@ -0,0 +1,32 @@
+package com.nlw.certification.questions;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name= "alternatives")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlternativesEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+
+    private boolean isCorrect;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+}
