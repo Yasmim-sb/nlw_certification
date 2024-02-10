c2ec7e32c5daca5d33525965dbdd7faed6e698a1
Yasmim-bs <yasmimsabrinacp@gmail.com>
2024-02-10 16:41:31 -0300

    feat: adding two resultDTO classes to manipulate and filter the data output in the response

--- src/main/java/com/nlw/certification/controllers/QuestionController.java
+++ src/main/java/com/nlw/certification/controllers/QuestionController.java
@@ -1,5 +1,8 @@
 package com.nlw.certification.controllers;
 
+import com.nlw.certification.dto.AlternativeResultDTO;
+import com.nlw.certification.dto.QuestionResultDTO;
+import com.nlw.certification.questions.AlternativesEntity;
 import com.nlw.certification.questions.QuestionEntity;
 import com.nlw.certification.repositories.QuestionRepository;
 import lombok.AllArgsConstructor;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/questions")
@@ -19,8 +23,34 @@ public class QuestionController {
 
 
     @GetMapping("/technology/{technology}")
-    public List<QuestionEntity> findByTechnology(@PathVariable String technology){
+    public List<QuestionResultDTO> findByTechnology(@PathVariable String technology){
 
-     return  this.questionRepository.findByTechnology(technology);
+    var result = this.questionRepository.findByTechnology(technology);
+
+    var toMap = result.stream().map(question -> mapQuestionToDTO(question))
+    .collect(Collectors.toList());
+    return toMap;
+    }
+
+    static QuestionResultDTO mapQuestionToDTO(QuestionEntity question){
+        var questionResultDTO = QuestionResultDTO.builder()
+        .id(question.getId())
+        .technology(question.getTechnology())
+        .description(question.getDescription()).build();
+
+        List<AlternativeResultDTO> alternativeResultDTOs = question.getAlternatives()
+        .stream().map(alternative -> mapAlternativeDTO(alternative))
+        .collect(Collectors.toList());
+
+        questionResultDTO.setAlternative(alternativeResultDTOs);
+        return questionResultDTO;
+    }
+
+    static AlternativeResultDTO mapAlternativeDTO(AlternativesEntity alternativesResultDTO){
+
+       return AlternativeResultDTO.builder()
+       .id(alternativesResultDTO.getId())
+        .description(alternativesResultDTO.getDescription()).build();
+
     }
 }
